295493339fce3e7409e7926347178bdfaf7b03fb
Beeraiah1 <beeraiahmandhadi426@gmail.com>
2023-09-05 15:07:53 +0530

    this is my first commit

---
+++ Age.java
@@ -0,0 +1,23 @@
+class Age
+{
+	public static void main(String[] args)
+	{
+		int age=54;
+		if(age<18)
+		{
+		System.out.println("minor");
+		}
+		else if(age>50)
+		{
+		System.out.println("seniour");
+		}
+		else if(age<50)
+		{
+		System.out.println("major");
+		}
+		else
+		{
+		System.out.println("not");
+		}
+	}
+}
---
+++ Andoperator.java
@@ -0,0 +1,15 @@
+class Andoperator
+{
+	public static void main(String[] args)
+	{
+		int a=15,b=8;
+		if((a>b) && (++b==9))//both conditions true then only execute if statement otherwise goto else part
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
